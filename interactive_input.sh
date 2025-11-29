7b099b566a207224d298cec898a300196ee767db
emirakinci <98558882+emirakinci@users.noreply.github.com>
2025-11-29 11:56:43 +0100

    Added 2 more exercises...

---
+++ interactive_input.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+echo "What is your name"
+read FIRST_NAME
+
+echo "What is your last name"
+read LAST_NAME
+
+echo "The user's info:" $FIRST_NAME $LAST_NAME
+
---
+++ posargu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+
+echo "The passed arguments are:"
+echo "0th:" $0
+echo "1st:" $1
+echo "2nd:" $2
